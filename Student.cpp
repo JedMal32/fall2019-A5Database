890a18d5a44bd70595f4f83cf7d8c9c3b8532692
Sabelle O'Connell <sabelleoconnell@sabelles-mbp.attlocal.net>
2019-11-25 04:19:29 -0800

    Student class edits

--- Student.cpp
+++ Student.cpp
@@ -4,31 +4,116 @@
 using namespace std;
 
 Student::Student(){
-
+  studentID = 0;
+  name = "";
+  level = "";
+  major = "";
+  gPA = 0.00;
+  facultyID = 0;
 }
 
-Student::Student(int i, string n, double g){
-  setID(i);
-  setName(n);
-  gPA = g;
+Student::Student(int id, string na, string le, string ma, double gp, int fa){
+  setStudentID(id);
+  setName(na);
+  setLevel(le);
+  setMajor(ma);
+  setGPA(gp);
+  setFID(fa);
 }
 
 Student::~Student(){
 
 }
 
-void Student::setFID(int i){
-  facultyID = i;
+void Student::printInfo(){
+  cout << "Student ID: " << studentID << endl;
+  cout << "Name: " << name << endl;
+  cout << "Level: " << level << endl;
+  cout << "Major: " << major << endl;
+  cout << "GPA: " << gPA << endl;
+  cout << "Faculty Advisor ID: " << facultyID << endl;
+}
+
+int Student::getStudentID(){
+  return studentID;
+}
+
+string Student::getName(){
+  return name;
+}
+
+string Student::getLevel(){
+  return level;
+}
+
+string Student::getMajor(){
+  return major;
+}
+
+double Student::getGPA(){
+  return gPA;
 }
 
 int Student::getFID(){
   return facultyID;
 }
 
-void Student::setGPA(double d){
+void Student::setStudentID(int id){
+  studentID = id;
+}
+
+void Student::setName(string na){
+  name = na;
+}
+
+void Student::setLevel(string le){
+  level = le;
+}
+
+void Student::setMajor(string ma){
+  major = ma;
+}
+
+void Student::setGPA(double gp){
   gPA = d;
 }
 
-double Student::getGPA(){
-  return gPA;
+void Student::setFID(int fa){
+  facultyID = fa;
+}
+
+bool Student::operator==(const Student &s){
+  if (studentID == s.getStudentID()){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+bool Student::operator!=(const Student &s){
+  if (studentID != s.getStudentID()){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+bool Student::operator<(const Student &s){
+  if (studentID < s.getStudentID()){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+bool Student::operator>(const Student &s){
+  if (studentID > s.getStudentID()){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
--- Student.h
+++ Student.h
@@ -5,17 +5,40 @@ using namespace std;
 
 class Student : public Affiliate{
 private:
+   int studentID = 0;
+   string name;
+   string level;
+   string major;
    double gPA = 0;
    int facultyID = 0;
 
 public:
    Student();
-   Student(int i, string n, double g);
+   Student(int id, string na, string le, string ma, double gp, int fa);
    ~Student();
 
-   void setFID(int i);
-   int getFID();
-   void setGPA(double d);
+   void printInfo();
+
+   //acccess
+   int getStudentID();
+   string getName();
+   string getLevel();
+   string getMajor();
    double getGPA();
+   int getFID();
+
+   //mutate
+   void setStudentID(int id);
+   void setName(string na);
+   void setLevel(string le);
+   void setMajor(string ma);
+   void setGPA(double gp);
+   void setFID(int fa);
+
+   //operators for comparison
+   bool operator==(const Student &s); // &s, if pointer
+   bool operator!=(const Student &s);
+   bool operator<(const Student &s);
+   bool operator>(const Student &s);
 
 };
